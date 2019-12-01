3d2acf1d2babb8c5a5f928d9c6ef05d46a5dca58
Thiago Melo <tbgdmelo@gmail.com>
2019-12-01 09:42:57 -0400

    Ediçao na URL para mostrar funcionario individual e lista com todos

--- backend/applavajato/urls.py
+++ backend/applavajato/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('select_report', views.select_report, name='tipo_relatorio'),
     path('cadastro_serv', views.cadastro_serv, name='cadastro_servico'),
     path('cadastro_nota', views.cadastro_nota, name='cadastro_nota'),
+    
     path('cadastro_func', views.cadastro_func, name='cadastro_funcionario'),
-    path('show_func', views.show_func, name='mostrar_funcionarios'),
+    path('show_all_func', views.show_all_func, name='mostrar_funcionarios'),
+    path('show_func/<int:matricula>', views.show_func, name='mostrar_um_funcionario'),
     path('edit_func/<int:matricula>', views.edit_func, name='editar_funcionario'),
     path('update_func/<int:matricula>', views.update_func, name='atualizar_funcionario'),
     path('delete_func/<int:matricula>', views.delete_func, name='deletar_funcionario')
